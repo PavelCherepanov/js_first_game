46c7870ce2add8650dcaa2641e2f2818827d3002
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-01-28 23:17:29 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var start = 0;
+// появляется фигура
+function makeShapeAppear(){
+  if (Math.random() > 0.5){
+  document.getElementById("shape").style.borderRadius = "50%";
+  } else{
+  document.getElementById("shape").style.borderRadius = "0%";
+  }
+// положение
+  var top = Math.random() * 400;
+  var left = Math.random() * 400;
+  document.getElementById("shape").style.top = top + "px";
+  document.getElementById("shape").style.left = left + "px";
+  // размер
+  var width = (Math.random() * 400) + 100;
+  var height = (Math.random() * 400) + 100;
+  document.getElementById("shape").style.width = width + "px";
+  document.getElementById("shape").style.height = height + "px";
+  // цвет
+  document.getElementById("shape").style.backgroundColor = getRandomColor();
+
+  document.getElementById("shape").style.display = "block";
+  start = new Date().getTime();
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+      // для задержки
+function appearAfterDelay(){
+  setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+
+appearAfterDelay();
+document.getElementById("shape").onclick = function(){
+  document.getElementById("shape").style.display = "none";
+  var end = new Date().getTime();
+  var timeTaken = (end - start) / 1000;
+  document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+  appearAfterDelay();
+}
